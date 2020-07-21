45faeb314e03d31e5678b9fa2c1090618978bd88
Jose Tavares <josetavares00@gmail.com>
2020-07-21 11:48:44 +0100

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `fullscreen`,
+        icon: 'src/images/Logo.webp'
       },
     },
     {
