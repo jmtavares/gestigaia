6ff13c7be3b685893fa191c79b947fba500f5a55
Jose Tavares <josetavares00@gmail.com>
2020-07-28 18:47:50 +0100

    remove service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,6 @@ module.exports = {
     `gatsby-plugin-emotion`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`    
+    //`gatsby-plugin-offline`    
   ],
 };
