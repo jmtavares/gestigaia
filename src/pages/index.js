ab6dfa4158ad706c9204f40bf2a4e59882daafef
Jose Tavares <josetavares00@gmail.com>
2022-01-03 10:02:03 +0000

    update lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,20 @@ import AboutUs from "../sections/about-us";
 import Services from "../sections/services";
 import ContactUs from "../sections/contact-us";
 
-const IndexPage = () => {
-  return (
-    <Layout>
-      <SEO
-        title="Gestigaia - Gabinete de Contabilidade | Seguros | Oliveira do Douro"
-        keywords={[]}
-      />
+const IndexPage = () => (
+  <Layout>
+    <SEO
+      title="Gestigaia - Gabinete de Contabilidade | Seguros | Oliveira do Douro"
+      keywords={[]}
+    />
 
-      <Header />
-      <Home />
-      <AboutUs />
-      <Services />
-      <ContactUs />
-      <Footer />
-    </Layout>
-  );
-};
+    <Header />
+    <Home />
+    <AboutUs />
+    <Services />
+    <ContactUs />
+    <Footer />
+  </Layout>
+);
 
 export default IndexPage;
